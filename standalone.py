858b6d5ea31ea2620f29f459c7d6ca8b382f7a98
aidatorajiro <kawarusosu@zoho.com>
2020-02-05 14:48:40 +0900

    downgrade to Electrum==3.3.8

--- standalone.py
+++ standalone.py
@@ -1,8 +1,10 @@
 # Standalone version simple electrum client.
+# compatible: Electrum==3.3.8
 
 import os
 import sys
 import asyncio
+import time
 
 from electrum.simple_config import SimpleConfig
 from electrum import constants
@@ -14,10 +16,8 @@ from electrum.network import filter_protocol, Network
 from electrum.util import create_and_start_event_loop, log_exceptions
 from electrum.simple_config import SimpleConfig
 
-loop, stopping_fut, loop_thread = create_and_start_event_loop()
-
 # use ~/.electrum/testnet as datadir
-config = SimpleConfig({"testnet": True, "fee_per_kb": 5000, "dynamic_fees": False})
+config = SimpleConfig({"testnet": True, "fee_per_kb": 10000, "dynamic_fees": False})
 
 # set testnet magic bytes
 constants.set_testnet()
@@ -29,43 +29,30 @@ network = daemon.network
 # get wallet on disk
 wallet_dir = os.path.dirname(config.get_wallet_path())
 print("Wallet dir: "+ wallet_dir)
-wallet_path = os.path.join(wallet_dir, "wallet_lib")
+wallet_path = os.path.join(wallet_dir, "wallet_lib2")
 if not os.path.exists(wallet_path):
-    create_new_wallet(path=wallet_path, config=config)
+    create_new_wallet(path=wallet_path, segwit=True)
 
 # open wallet
 storage = WalletStorage(wallet_path)
-wallet = Wallet(storage, config=config)
+wallet = Wallet(storage)
 wallet.start_network(network)
-daemon.add_wallet(wallet)
+# daemon.add_wallet(wallet)
 
 # set up commands
-commands = Commands(config=config, daemon=daemon, network=network)
+commands = Commands(config, wallet=wallet, network=network)
 
-def payto(*args):
-    return {'hex': commands._run('payto', args, wallet_path=wallet_path)}
+def create_func(key):
+    def f(*args):
+        return commands._run(key, args, lambda: "")
+    return f
 
-def broadcast(*args):
-    return commands._run('broadcast', args)
+command_set = {}
 
-def createnewaddress(*args):
-    return commands._run('createnewaddress', args, wallet_path=wallet_path)
-
-def getprivatekeys(*args):
-    return commands._run('getprivatekeys', args, wallet_path=wallet_path)
-
-command_set = {
-    'payto': payto,
-    'broadcast': broadcast,
-    'createnewaddress': createnewaddress,
-    'getprivatekeys': getprivatekeys
-}
+for k in known_commands.keys():
+    command_set[k] = create_func(k)
 
 import elm_nosig
 import elm_sig
 
-print("elm_sig starting....")
-
-elm_sig.elm_sig(command_set, "びっとこいん　めもようし　あどれす　ビットコインメモ用紙アドレス bitcoinメモ用紙アドレス", "tb.......", 20000, 10)
-
-print("elm_sig completed....")
+# do whatever you want
